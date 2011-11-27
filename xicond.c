cc0ded44f43ff39c5a4e85072bb18a70fcf4e3c9
Chen Yan <raffel.chen7@gmail.com>
2011-11-26 22:29:07 -0500

    need some cleaning ...

--- xicond.c
+++ xicond.c
@@ -6,6 +6,7 @@
 #include <gdk-pixbuf/gdk-pixbuf.h>
 #include <librsvg/rsvg.h>
 #include <gtk/gtk.h>
+#include <unistd.h>
 int verbose = 1;
 
 typedef unsigned long int CARD32;
@@ -76,81 +77,78 @@ int main(int argc, char **argv){
     gicontheme = gtk_icon_theme_get_default();
     giconlist = gtk_icon_theme_list_icons(gicontheme, "Applications");
     /*
-    p = giconlist;
-    while (p != NULL){
-        printf("icon - %s\n", p->data);
-        p = p->next;
-        //g_free(giconlist);
-        giconlist = p;
+       p = giconlist;
+       while (p != NULL){
+       printf("icon - %s\n", p->data);
+       p = p->next;
+    //g_free(giconlist);
+    giconlist = p;
     }
-    
+
     exit(1);*/
     XSelectInput(dpy, DefaultRootWindow(dpy), SubstructureNotifyMask);
     for (;;){
         XNextEvent(dpy, &e);
-        if (e.type == CreateNotify){
-            wid = e.xcreatewindow.window;
-            int status = XGetWindowProperty(
-                    dpy,
-                    wid,
-                    XInternAtom(dpy, "_NET_WM_ICON_NAME", True),
-                    0,
-                    (~0L),
-                    False,
-                    AnyPropertyType,
-                    &actual_type,
-                    &actual_format,
-                    &nitems,
-                    &bytes,
-                    &name);
-            if (name){ 
-                printf("icon name = %s\n", name);
-                printf("at %d,%d of %d*%d: parent=0x%x, window=0x%x\n", 
-                        e.xcreatewindow.x, 
-                        e.xcreatewindow.y, 
-                        e.xcreatewindow.width, 
-                        e.xcreatewindow.height, 
-                        (int) e.xcreatewindow.parent, 
-                        (int) wid);
-                //XFree(name); 
-                
-                Atom *proplist;
-                Atom iconatom;
-                int n_prop;
-                iconatom = XInternAtom(dpy, "_NET_WM_ICON", 0);
-                proplist = XListProperties(dpy, wid, &n_prop);
-                for (i=0; i<n_prop && proplist[i] != iconatom; i++)
-                    printf("%s vs %s\n", XGetAtomName(dpy, proplist[i]), 
-                            XGetAtomName(dpy, iconatom));
-                if (i == n_prop){
-                    printf("this window has no icon\n");
-                    continue;
-                }
-                XFree(proplist);
-                CARD32 *iconcard32;
-                GdkPixbuf *iconpix;
-                GError *err = NULL;
-                iconpix = gtk_icon_theme_load_icon(gicontheme, name, 128, 0, &err);
-                printf("icon %s at %p\n", name, iconpix);
-                //iconpix = rsvg_pixbuf_from_file_at_size("./abiword.svg", 48, 48, &err);
-                printf("iconpix @ %p\n", iconpix);
-                if (iconpix == NULL) continue;
-                iconcard32 = pixbuf2card32(iconpix);
-                int success = XChangeProperty(
-                        dpy,
-                        wid,
-                        iconatom,
-                        XA_CARDINAL,
-                        32,
-                        PropModeReplace,
-                        (gchar*)iconcard32,
-                        48 * 48 + 2);
-                if (success != NULL)
-                    printf("image: %dx%d\n", *(iconcard32), *(iconcard32+1));
-                printf("#655pixel=%d\n", *(iconcard32+655));
-                //g_free(iconcard32);
-                
-            }
+        if (e.type != CreateNotify) continue;
+        wid = e.xcreatewindow.window;
+        int status = XGetWindowProperty(
+                dpy, wid, XInternAtom(dpy, "_NET_WM_ICON_NAME", True),
+                0, (~0L), False, AnyPropertyType,
+                &actual_type, &actual_format, &nitems, &bytes, &name);
+        if (name == NULL) continue;
+        printf("icon name = %s\n", name);
+        printf("at %d,%d of %d*%d: parent=0x%x, window=0x%x\n", 
+                e.xcreatewindow.x, 
+                e.xcreatewindow.y, 
+                e.xcreatewindow.width, 
+                e.xcreatewindow.height, 
+                (int) e.xcreatewindow.parent, 
+                (int) wid);
+        //XFree(name); 
+
+        Atom *proplist;
+        Atom iconatom;
+        int n_prop = 0;
+        i = 0;
+        iconatom = XInternAtom(dpy, "_NET_WM_ICON", False);
+        while (i >= n_prop){
+            proplist = XListProperties(dpy, wid, &n_prop);
+            for (i=0; i<n_prop && proplist[i] != iconatom; i++);
+            //XFree(proplist);
+            usleep(100);
+        }
+        if (i == n_prop) {
+            printf("this window has no icon\n");
+            //continue;
+        }
+        XFree(proplist);
+        CARD32 *iconcard32;
+        GdkPixbuf *iconpix;
+        GError *err = NULL;
+        iconpix = gtk_icon_theme_load_icon(gicontheme, name, 48, 0, &err);
+        printf("icon %s at %p\n", name, iconpix);
+        //iconpix = rsvg_pixbuf_from_file_at_size("./abiword.svg", 48, 48, &err);
+        if (iconpix == NULL) continue;
+        iconcard32 = pixbuf2card32(iconpix);
+        status = XGetWindowProperty(
+                dpy, wid, XInternAtom(dpy, "_NET_WM_ICON_NAME", True),
+                0, (~0L), False, AnyPropertyType,
+                &actual_type, &actual_format, &nitems, &bytes, &name);
+        printf("icon - name 2: %s\n", name);
+
+        int success = XChangeProperty(
+                dpy,
+                wid,
+                iconatom,
+                XA_CARDINAL,
+                32,
+                PropModeReplace,
+                (gchar*)iconcard32,
+                48 * 48 + 2);
+        if (success != NULL){
+            printf("image: %dx%d\n", *(iconcard32), *(iconcard32+1));
+            //g_free(iconcard32);
+
         }
     }
     return 0;
